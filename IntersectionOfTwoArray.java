ca22e72506e46789af7b19495934d8c15675e65b
poojadas09 <poojadas053@gmail.com>
2024-08-04 23:20:54 +0530

    deleted few production codes

--- IntersectionOfTwoArray.java
+++ IntersectionOfTwoArray.java
@@ -4,9 +4,9 @@ public class IntersectionOfTwoArray {
     public static void main(String[] args) {
         int[] firstArray = {1, 2, 3, 2};
         int[] secondArray = {1, 3, 4, 6, 2, 3, 2};
-        System.out::println);Arrays.stream(firstArray).filter(
+        Arrays.stream(firstArray).filter(
                         x -> Arrays.stream(secondArray).anyMatch(
                                 y -> y == x)).distinct()
-                .forEach(
+                .forEach(System.out::println);
     }
 }
