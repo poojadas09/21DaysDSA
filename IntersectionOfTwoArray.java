b39d598db89c680babf488c8084ddc3d9e40fa22
poojadas09 <poojadas053@gmail.com>
2024-08-04 23:17:49 +0530

    initial commit

---
+++ IntersectionOfTwoArray.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class IntersectionOfTwoArray {
+    public static void main(String[] args) {
+        int[] firstArray = {1, 2, 3, 2};
+        int[] secondArray = {1, 3, 4, 6, 2, 3, 2};
+        System.out::println);Arrays.stream(firstArray).filter(
+                        x -> Arrays.stream(secondArray).anyMatch(
+                                y -> y == x)).distinct()
+                .forEach(
+    }
+}
